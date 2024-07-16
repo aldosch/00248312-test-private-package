81a82cdb981ed4d5f4a45b5f016e1d3964141124
aldosch <git@aldo.io>
2024-07-16 15:54:48 +1000

    add echos to see progress

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+echo "👀 Cloning private package"
 git clone $PRIVATE_PACKAGE_AUTH_URL
+echo "🚶‍♀️‍➡️ navigating to private package dir"
 cd private-test-package
+echo "👀 listing files in private package dir"
+ls
+echo "🔗 linking private package"
 npm link
+echo "👋 exiting private package dir" 
 cd ..
+echo "👀 listing files in root dir"
+ls
+echo "🔗 linking private package"
 npm link private-test-package
+echo "📦 installing dependencies"
 npm install
