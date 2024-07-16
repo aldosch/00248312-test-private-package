e5ccb9057e0101c8848dde909391bfa1e0aa2e00
aldosch <git@aldo.io>
2024-07-16 15:29:54 +1000

    clone private package into current folder

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-pwd
-# cd ..
-# git clone $PRIVATE_PACKAGE_AUTH_URL
-# cd private-test-package
-# npm link
-# cd ../00248312-test-private-package
-# npm link private-test-package
-# npm install
+git clone $PRIVATE_PACKAGE_AUTH_URL
+cd private-test-package
+npm link
+cd ..
+npm link private-test-package
+npm install
