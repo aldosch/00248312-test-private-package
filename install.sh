20630dc4ac6da412682d2e6f604ec21d26fac11b
aldosch <git@aldo.io>
2024-07-16 14:23:08 +1000

    rm unused touch

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.ssh
 echo -e $GIT_PRIVATE_SSH_KEY > ~/.ssh/id_ed25519
 echo -e $GIT_SSH_KNOWN_HOSTS >> ~/.ssh/known_hosts
-chmod 600 ~/.ssh/id_ed25519
 mv -i config ~/.ssh/
+chmod 600 ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/config
-touch
+chmod 600 ~/.ssh/known_hosts
 git clone git@github.com:aldosch/private-test-package.git
