a340cc3789b08530847396e643de5794d58a78f3
aldosch <git@aldo.io>
2024-07-16 16:13:08 +1000

    rm echos, add bang, add set -e to end script on failure

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
-echo "👀 Cloning private package"
+#!/bin/bash
+set -e
 git clone $PRIVATE_PACKAGE_AUTH_URL
-echo "🚶‍♀️‍➡️ navigating to private package dir"
 cd private-test-package
-echo "👀 listing files in private package dir"
 ls
-echo "🔗 linking private package"
 npm link
-echo "👋 exiting private package dir" 
 cd ..
-echo "👀 listing files in root dir"
 ls
-echo "🔗 linking private package"
 npm link private-test-package
-echo "📦 installing dependencies"
 npm install
