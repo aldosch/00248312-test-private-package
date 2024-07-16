267a5bf575a4964c49bbe2ec512745d544ddc678
aldosch <git@aldo.io>
2024-07-16 14:21:27 +1000

    add known_hosts

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 mkdir -p ~/.ssh
 echo -e $GIT_PRIVATE_SSH_KEY > ~/.ssh/id_ed25519
+echo -e $GIT_SSH_KNOWN_HOSTS >> ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_ed25519
 mv -i config ~/.ssh/
 chmod 600 ~/.ssh/config
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+touch
 git clone git@github.com:aldosch/private-test-package.git
