a33fdd28c159e144f9774c0a93c2a58eb01bc387
aldosch <git@aldo.io>
2024-07-16 15:27:59 +1000

    temp pwd

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-cd ..
-git clone $PRIVATE_PACKAGE_AUTH_URL
-cd private-test-package
-npm link
-cd ../00248312-test-private-package
-npm link private-test-package
-npm install
+pwd
+# cd ..
+# git clone $PRIVATE_PACKAGE_AUTH_URL
+# cd private-test-package
+# npm link
+# cd ../00248312-test-private-package
+# npm link private-test-package
+# npm install
